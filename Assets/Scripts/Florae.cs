2594ee04c9ad4f97bf2b0aa082ca09b37ecbefed
Jesin Jarod Martinez <jesinmartinez21@gmail.com>
2021-04-25 15:33:07 +0800

    Asset Commit

---
+++ Assets/Scripts/Florae.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Florae : MonoBehaviour
+{
+    [Header("Main Components")]
+    [SerializeField] Image flowerImage = null;
+
+    public void Setup() 
+    {
+        
+    }
+}
